28f760c5a26ba279c26711f58afbf54d9fb5b2c1
roistaff <roistaff1983@gmail.com>
2024-03-19 16:23:30 +0900

    test build

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/roistaff/hello-telnet
+
+go 1.21.4
+
+require github.com/reiver/go-telnet v0.0.0-20180421082511-9ff0b2ab096e
+
+require github.com/reiver/go-oi v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "os"
+    "os/signal"
+    "syscall"
+    "time"
+
+    telnet "github.com/reiver/go-telnet"
+)
+
+var counter int = 0
+
+func main() {
+    var handler telnet.Handler = MainHandler{}
+    fmt.Println("start server")
+    go func() {
+        err := telnet.ListenAndServe(":23", handler)
+        if nil != err {
+            panic(err)
+        }
+    }()
+    ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, os.Interrupt, os.Kill)
+    defer stop()
+    <-ctx.Done()
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+}
+type MainHandler struct{}
+
+func (handler MainHandler) ServeTELNET(ctx telnet.Context, w telnet.Writer, r telnet.Reader) {
+    str := `
+Hello World from Internet.I am netrunnner.
+`
+        w.Write([]byte(str))
+    }
